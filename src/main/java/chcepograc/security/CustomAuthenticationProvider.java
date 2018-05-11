56e324c866fd507934f9fb380a0e49effeaa011c
Tomasz Chojna <tomek.chojna@gmail.com>
2018-05-11 05:08:53 +0200

    Handle login

---
+++ src/main/java/chcepograc/security/CustomAuthenticationProvider.java
@@ -0,0 +1,52 @@
+package chcepograc.security;
+
+import chcepograc.models.User;
+import chcepograc.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Transactional
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String name = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        Optional<User> user = userRepository.findByEmail(name);
+        if (!user.isPresent()) {
+            throw new BadCredentialsException("Email or password invalid");
+        }
+
+        User foundUser = user.get();
+        if (!foundUser.checkPassword(password)) {
+            throw new BadCredentialsException("Email or password invalid");
+        }
+
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+        grantedAuthorities.add(new SimpleGrantedAuthority("USER"));
+
+        return new UsernamePasswordAuthenticationToken(foundUser.getEmail(), foundUser.getPassword(), grantedAuthorities);
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
